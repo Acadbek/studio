e0d6847d0ef0b8a69da5a1c9bb5ad3996927e8c0
Asadbek <a.nosirjonov@uic.group>
2022-10-14 18:23:47 +0500

    use: lazyLoadImage for performance

--- src/components/generic/banner/index.jsx
+++ src/components/generic/banner/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import bannerPhoto from "../../../assets/images/bannerphoto.jpg";
 import { Blur, Container, Title } from "./style";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+// import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Banner = ({ img, title, heightSize, widthSize }) => {
   return (
     <Container heightSize={heightSize} widthSize={widthSize}>
       <Blur />
+      {/* <LazyLoadImage
+        height={500}
+        width={100}
+        delayTime={500}
+        src={img || bannerPhoto}
+        alt="banner"
+      /> */}
       <img height={500} width={100} src={img || bannerPhoto} alt="banner" />
       <Title>{title || "Never worry about content again. Seriously."}</Title>
     </Container>
